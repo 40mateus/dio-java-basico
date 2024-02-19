fab56fd05f385f1bb14d38127ad22607229090a8
40mateus <mtheusoliveira29@gmail.com>
2024-02-19 12:55:00 -0300

    Meu primeiro projeto Simulando uma Conta de Banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       // Conhecer e Importar a classe Scanner 
+        // Importa a classe Scanner para permitir a entrada do usuário
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o nosso usuário 
+        System.out.println("Bem-vindo ao sistema de criação de contas!");
+
+        // Exibir mensagem solicitando entrada de dados
+        System.out.println("Por favor, digite seu nome:");
+
+        // obter pela Scanner os valores digitados no terminal
+        // Lê o nome fornecido pelo usuário
+        String nome = scanner.nextLine();
+
+        // Exibir mensagem informando que a conta foi criada com sucesso
+        System.out.println("Conta criada com sucesso para " + nome + "!");
     }
 }
